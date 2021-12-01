8f6c2204f719d1fb2b10b1161d168ea4fd138b94
cyril delmas <cyril.delmas@gmail.com>
2021-12-01 22:18:01 +0100

    test: use proptest instead of quickcheck

--- src/lists/mod.rs
+++ src/lists/mod.rs
@@ -35,61 +35,113 @@ fn is_palindrome<A: Eq>(l: &Vec<A>) -> bool {
     forward.zip(backward).all(|(a, b)| b == a)
 }
 
+#[derive(Clone)]
+enum NestedList<A> {
+    Elem(A),
+    List(Vec<Box<NestedList<A>>>),
+}
+
+fn flatten<A>(list: &NestedList<A>) -> Vec<&A> {
+    match list {
+        NestedList::Elem(e) => vec![e],
+        NestedList::List(_) => vec![],
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
-    use quickcheck::TestResult;
-    use quickcheck_macros::*;
+    use proptest::prelude::*;
+    use NestedList::*;
 
     #[test]
     fn it_does_pass() {
         assert_eq!(1, 1);
     }
 
-    #[quickcheck]
-    fn last_is_reverse_head(list: Vec<isize>) -> bool {
-        let mut reversed = list.clone();
-        reversed.reverse();
-        reversed.first() == last(&list)
-    }
-
-    #[quickcheck]
-    fn but_last_is_reverse_second(list: Vec<isize>) -> bool {
-        let mut reversed = list.clone();
-        reversed.reverse();
-        reversed.get(1) == but_last(&list)
-    }
-
-    #[quickcheck]
-    fn kth_with_index_bigger_than_size_is_none(list: Vec<isize>, idx: usize) -> TestResult {
-        if idx < list.len() + 1 && idx != 0 {
-            TestResult::discard()
-        } else {
-            TestResult::from_bool(k_th(&list, idx).is_none())
+    proptest! {
+        #[test]
+        fn last_is_reverse_head(list: Vec<isize>) {
+            let mut reversed = list.clone();
+            reversed.reverse();
+            prop_assert_eq!(reversed.first(), last(&list));
         }
     }
 
-    #[quickcheck]
-    fn kth_with_index_in_range_1_len_is_some(list: Vec<isize>, idx: usize) -> TestResult {
-        if idx == 0 || idx > list.len() {
-            TestResult::discard()
-        } else {
-            TestResult::from_bool(k_th(&list, idx).is_some())
+    proptest! {
+        #[test]
+        fn but_last_is_reverse_second(list: Vec<isize>) {
+            let mut reversed = list.clone();
+            reversed.reverse();
+            prop_assert_eq!(reversed.get(1), but_last(&list));
         }
     }
 
-    #[quickcheck]
-    fn reverse_the_reversed_list_gives_the_original(list: Vec<isize>) -> bool {
-        reverse(&reverse(&list)) == list
+    #[derive(Clone)]
+    enum IndexRange {
+        OutOfBounds,
+        InBounds,
     }
 
-    #[quickcheck]
-    fn concatenate_the_reversed_is_a_palindrom(list: Vec<isize>) -> bool {
-        let mut copy = list.clone();
-        copy.reverse();
-        let mut v = list.clone();
-        v.append(&mut copy);
-        is_palindrome(&v)
+    fn vec_and_index(range: IndexRange) -> impl Strategy<Value = (Vec<isize>, usize)> {
+        prop::collection::vec(any::<isize>(), 1..100).prop_flat_map(move |vec| {
+            let len = vec.len();
+            let index_range = match range.clone() {
+                IndexRange::OutOfBounds => len..usize::MAX,
+                IndexRange::InBounds => 0..len,
+            };
+            (Just(vec), index_range)
+        })
+    }
+
+    proptest! {
+        #[test]
+        fn kth_with_index_bigger_than_size_is_none(list_and_idx in vec_and_index(IndexRange::OutOfBounds)) {
+            let (list, idx) = list_and_idx;
+            prop_assert!(k_th(&list, idx).is_none());
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn kth_with_index_in_range_1_len_is_some(list_and_idx in vec_and_index(IndexRange::InBounds)) {
+            let (list, idx) = list_and_idx;
+            prop_assert!(k_th(&list, idx+1).is_some()); // NOTE: idx+1 is because the generator starts with 0 as lower bound, but we want 1.
+        }
+    }
+
+    proptest! {
+        #[test]
+        fn reverse_the_reversed_list_gives_the_original(list: Vec<isize>) {
+            prop_assert_eq!(reverse(&reverse(&list)), list);
+        }
+    }
+
+    #[test]
+    fn is_a_palindrom() {
+        assert_eq!(is_palindrome(&vec![1, 2, 4, 2, 1]), true);
+    }
+
+    proptest! {
+        #[test]
+        fn concatenate_the_reversed_is_a_palindrom(list: Vec<isize>) {
+            let mut copy = list.clone();
+            copy.reverse();
+            let mut v = list.clone();
+            v.append(&mut copy);
+            prop_assert!(is_palindrome(&v));
+        }
+    }
+
+    // TODO: implement a strategy for NestedList
+
+    #[test]
+    fn flatten_an_elem_is_a_vector_with_the_elem() {
+        let e = Elem(3);
+
+        let result = flatten(&e);
+
+        assert_eq!(result, vec![&3]);
     }
 }
